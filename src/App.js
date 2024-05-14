b77751b86d7bac720607ceb42c173f84042aae84
Anton Indyukov <indyukovanton@gmail.com>
2024-05-15 02:47:03 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <p className="cash"></p>
+        <img src='./images/assets/icon-settings.svg' className="settings logo" alt='settings'></img>
+      </header>
+      <main className="main"></main>
+      <footer className="footer"></footer>
+    </div>
+  );
+}
+
+export default App;
